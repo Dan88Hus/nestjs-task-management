e3c09003c487ea04828fc346c396f884d5c6c08b
Huseyin OZDOGAN <huseyinozdogan@gmail.com>
2021-07-05 20:56:55 +0300

    updateTaskStatus

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -30,4 +30,10 @@ export class TasksService {
     deleteTask(id): void {
          this.tasks = this.tasks.filter((task)=> task.id !== id)
     }
+
+    updateTaskStatus(id:string, status: TaskStatus): Task{
+        const task = this.getTaskById(id)
+        task.status = status
+        return task
+    }
 }
