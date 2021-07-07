6a25282e0009582ee1d20cfdf2d9f9328562880a
Huseyin OZDOGAN <huseyinozdogan@gmail.com>
2021-07-07 13:30:16 +0300

    hash password with bcrypt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,17 +2,23 @@ import { ConflictException, HttpException, InternalServerErrorException } from "
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
+import * as bcrypt from 'bcrypt'
 
 @EntityRepository(User) //this UserRepository interactcs with user DB
 export class UserRepository extends Repository<User>{
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void>{
         const { username, password } = authCredentialsDto
+
+        // console.log(salt)
      
         const user = new User() //this is new entity we create
         user.username = username
-        user.password = password 
+        user.salt = await bcrypt.genSalt()
+        user.password = await this.hashPassword(password, user.salt) 
+        // console.log(user.password)
         try {
             await user.save()
+            // console.log("user save is ready")
             
         } catch (error) {
             // console.log(error.code) // @Unique validation error code is "23505"
@@ -23,6 +29,8 @@ export class UserRepository extends Repository<User>{
             }
         }
         //if we not return anything its void type as well
-
+    }
+    private async hashPassword(password: string, salt: string): Promise<string>{
+        return bcrypt.hash(password, salt)
     }
 }
