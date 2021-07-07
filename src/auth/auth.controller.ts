e0463af1d6ace6737ef867e54ac1a9cc1c69f63b
Huseyin OZDOGAN <huseyinozdogan@gmail.com>
2021-07-07 09:47:05 +0300

    password validation wit regex

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
@@ -7,7 +7,7 @@ export class AuthController {
     constructor(private authService: AuthService){}
     
     @Post("/signup") ///auth/signup
-    async singUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void>{
+    async singUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void>{
         // console.log(authCredentialsDto) //now we need to return service
         return await this.authService.signup(authCredentialsDto)
     }
--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,24 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator"
+
+
 export class AuthCredentialsDto{
+    @IsString() //validation
+    @MinLength(4)
+    @MaxLength(20)
     username: string
+
+    @IsString() 
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    {message: "Password too weak"})
     password: string
+//     strong-password-regex.md
+// Passwords will contain at least 1 upper case letter
+// Passwords will contain at least 1 lower case letter
+// Passwords will contain at least 1 number or special character
+// There is no length validation (min, max) in this regex!
+// Regular expression for JavaScript:
+
+// /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
 }
